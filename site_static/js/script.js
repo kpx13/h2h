6fe07fd5cf53075d0c60918d33eab19cc10516c7
Ann Kpx <annkpx@gmail.com>
2013-10-03 17:15:29 +0400

    правка вёрстки

--- site_static/js/script.js
+++ site_static/js/script.js
@@ -1,27 +1,39 @@
-$(function(){
-    var service =  $(".services ul").find('li');
-    service.click(function(){
-        $(this).toggleClass("_active");
-    });
+$(function () {
     var selectBtn = $("<div class='select-btn'></div>");
-      selectBtn.appendTo(".select");
+    selectBtn.appendTo(".select");
 
-    $('nav ul li').hover(function(){
-         $(this).find('ul').slideToggle();
+    $('nav ul li').mouseover(function () {
+        var item = $(this).find('ul');
+        if (!item.is(':animated')) {
+            if (!$(this).hasClass("active")) {
+                item.slideDown();
+                $(this).addClass("active");
+            }
+        }
     });
-    var reviewWrap =  $('<div class="review__popup__wrap"></div>'),
+    $('nav ul li').mouseleave(function () {
+        var item = $(this).find('ul');
+        if (!item.is(':animated')) {
+            if ($(this).hasClass("active")) {
+                item.slideUp();
+                $(this).removeClass("active");
+            }
+
+        }
+    });
+    var reviewWrap = $('<div class="review__popup__wrap"></div>'),
         review__block = $('.review__popup'),
         btnSend = $('.review__btn._send');
     review__block.appendTo('body');
     reviewWrap.appendTo('body');
 
-    $(".review__btn").click(function(){
+    $(".review__btn").click(function () {
         review__block.fadeIn();
         reviewWrap.fadeIn();
         return false;
     });
 
-    $(".review__close, .review__popup__wrap, .review__btn._send").click(function(){
+    $(".review__close, .review__popup__wrap, .review__btn._send").click(function () {
         review__block.fadeOut();
         reviewWrap.fadeOut();
     });
