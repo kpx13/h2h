cbde54b1b5be4285a998bcc887d0ae594341e7b0
JT <tanner.of.kha@gmail.com>
2014-07-28 14:52:19 +1100

    Blank=true

--- gallery/models.py
+++ gallery/models.py
@@ -9,8 +9,8 @@ class Category(models.Model):
     title = models.CharField(max_length=128, verbose_name=u'название')
     year = models.IntegerField(verbose_name=u'год')
     country = models.ForeignKey(Country, verbose_name=u'страна')
-    preview_image = models.ImageField(upload_to='uploads/gallery', max_length=256, verbose_name=u'превью')
-    text = RichTextField(null=True, verbose_name=u'описание галереи')
+    preview_image = models.ImageField(blank=True, null=True, upload_to='uploads/gallery', max_length=256, verbose_name=u'превью')
+    text = RichTextField(blank=True, null=True, verbose_name=u'описание галереи')
 
     class Meta:
         verbose_name = u'категория'
@@ -23,11 +23,11 @@ class Category(models.Model):
 
 class Photo(models.Model):
     category = models.ForeignKey(Category, verbose_name=u'категория', related_name='photos')
-    image = models.ImageField(upload_to='uploads/gallery', max_length=256, null=True, verbose_name=u'картинка')
-    video = EmbedVideoField(max_length=256, null=True, verbose_name=u'видео')
+    image = models.ImageField(blank=True, null=True, upload_to='uploads/gallery', max_length=256, verbose_name=u'картинка')
+    video = EmbedVideoField(blank=True, null=True, max_length=256, verbose_name=u'видео')
     date = models.DateField(auto_now_add=True, blank=True, verbose_name=u'дата написания')
     sort = models.IntegerField(default=100, verbose_name=u'порядок при сортировке')
-    text = RichTextField(null=True, verbose_name=u'описание фото')
+    text = RichTextField(blank=True, null=True, verbose_name=u'описание фото')
 
     class Meta:
         verbose_name = u'фотография'
@@ -36,4 +36,3 @@ class Photo(models.Model):
 
     def __unicode__(self):
         return str(self.id)
-
